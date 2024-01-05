8867b2ae81bab228d12278f1e0c49862bb7ed07c
erentrk01 <68688967+erentrk01@users.noreply.github.com>
2024-01-05 13:58:38 +0300

    Sensor data processing done

---
+++ lambda/index.ts
@@ -0,0 +1,40 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { PutCommand } from '@aws-sdk/lib-dynamodb';
+import { randomUUID } from 'crypto';
+
+const dynamodb = new DynamoDB({});
+
+interface SensorData {
+  "Humidity": number,
+  "Temperature Celcius": number,
+  "Temperature Fahrenheit": number,
+  "Light Intensity": number,
+  "Soil Moisture": number,
+  "CO": number
+}
+
+export const handler = async (event: any, context: any): Promise<void> => {
+  try {
+    console.log("HELLO EVENT: ");
+    console.log(JSON.stringify(event, null, 2));
+
+    // Directly access the properties of the event object
+    const sensorData: SensorData = event;
+
+    // Additional logic for processing and storing data in DynamoDB
+    const uuid = randomUUID();
+    await dynamodb.send(
+      new PutCommand({
+        TableName: process.env.TABLE_NAME,
+        Item: {
+          pk: `POST#${uuid}`,
+          sk: new Date().toISOString(),
+          ...sensorData,
+        },
+      })
+    );
+  } catch (error) {
+    console.error('Error processing sensor data:', error);
+    throw error;
+  }
+};
--- lib/flora_vision_cloud-stack.ts
+++ lib/flora_vision_cloud-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_lambda } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class FloraVisionCloudStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+	
 
-    // The code that defines your stack goes here
+   // Create DynamoDB table
+    const sensorDataTable = new Table(this, 'SensorDataTable', {
+      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      sortKey: { name: 'sk', type: AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Don't use in production!
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FloraVisionCloudQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+	// Create Lambda function
+    const lambdaFunction = new  NodejsFunction(this, 'SensorDataProcessor', {
+	  entry: 'lambda/index.ts',
+	  handler: 'handler',
+      runtime: aws_lambda.Runtime.NODEJS_18_X,
+      environment: {
+        TABLE_NAME: sensorDataTable.tableName,
+      },
+    });
+
+	// Grant Lambda permissions to interact with DynamoDB
+    sensorDataTable.grantReadWriteData(lambdaFunction);
+
   }
 }
