540cd2f5d19ae3e21b7f7d565da8a25e518d54e6
erentrk01 <68688967+erentrk01@users.noreply.github.com>
2024-02-03 13:22:23 +0300

    Websocket API Lambda Authorizer Done

--- lambda/connect.ts
+++ lambda/connect.ts
@@ -2,6 +2,7 @@ import { ApiGatewayManagementApi } from 'aws-sdk';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { PutCommand } from '@aws-sdk/lib-dynamodb';
 import {
+	APIGatewayProxyHandler,
 	APIGatewayProxyResultV2,
 	APIGatewayProxyWebsocketEventV2,
 	APIGatewayProxyWebsocketHandlerV2,
@@ -10,18 +11,19 @@ import {
 
 const dynamodb = new DynamoDB({});
 
-export const handler: APIGatewayProxyWebsocketHandlerV2 = async (event: APIGatewayProxyWebsocketEventV2): Promise<APIGatewayProxyResultV2> => {
-  const connectionId = event.requestContext.connectionId;
+export const handler: APIGatewayProxyHandler= async (event) => {
+  const connectionId = event.requestContext.connectionId!;
   const payload = JSON.parse(event.body || '{}'); // Assuming device ID is included in the payload
   const deviceId = payload.deviceId || '';
+  const userId = event.requestContext.authorizer!.userId;
 
   // Save the connection ID and associate it with the device ID in DynamoDB
-  await saveConnectionId(deviceId, connectionId);
+  await saveConnectionId(deviceId, connectionId, userId);
 
   return { statusCode: 200, body: 'Connected.' };
 };
 
-const saveConnectionId = async (deviceId: string, connectionId: string): Promise<void> => {
+const saveConnectionId = async (deviceId: string, connectionId: string, userId:any): Promise<void> => {
   try {
     await dynamodb.send(
 		new PutCommand({
@@ -29,6 +31,7 @@ const saveConnectionId = async (deviceId: string, connectionId: string): Promise
 		  Item: {
 			deviceId, 
 			connectionId ,
+			userId
 		  },
 		})
 	  );
--- lib/flora_vision_cloud-stack.ts
+++ lib/flora_vision_cloud-stack.ts
@@ -1,18 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_lambda } from 'aws-cdk-lib';
+import { aws_lambda, aws_dynamodb as dynamo } from 'aws-cdk-lib';
+import { aws_cognito as cognito, RemovalPolicy } from "aws-cdk-lib";
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as apigw2 from '@aws-cdk/aws-apigatewayv2-alpha';
 import * as apigw2Integrations from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import * as agwa from "@aws-cdk/aws-apigatewayv2-authorizers-alpha";
 
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+
+
 
 export class FloraVisionCloudStack extends cdk.Stack {
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 	
+	const userPool = new cognito.UserPool(this, 'UserPool', {
+		selfSignUpEnabled: true,
+		autoVerify: {
+		  email: true,
+		},
+		removalPolicy: RemovalPolicy.DESTROY,
+	  });
+
+	  const client = userPool.addClient('Client', {
+		authFlows: {
+		  userPassword: true,
+		  userSrp: true,
+		},
+	  });
+
+	// Access the user pool and client as needed
+    const userPoolId = userPool.userPoolId;
+    const clientId = client.userPoolClientId;
 
    // Create DynamoDB tables
     const sensorDataTable = new Table(this, 'SensorDataTable', {
@@ -27,6 +49,11 @@ export class FloraVisionCloudStack extends cdk.Stack {
 		removalPolicy: cdk.RemovalPolicy.DESTROY, // Don't use in production!
 		billingMode: BillingMode.PAY_PER_REQUEST,
 		
+	  });
+
+	  wsConnectionTable.addGlobalSecondaryIndex({
+		partitionKey: { name: "userId", type: dynamo.AttributeType.STRING },
+		indexName: "userIdIndex",
 	  });
   
 
@@ -44,7 +71,18 @@ export class FloraVisionCloudStack extends cdk.Stack {
     sensorDataTable.grantReadWriteData(lambdaFunction);
 
 
+	const authLambda =new NodejsFunction(this, 'AuthLambda', {
+		entry: 'lambda/authorizer.ts',
+		handler: 'handler',
+		runtime: aws_lambda.Runtime.NODEJS_18_X,
+		environment: {
+			USER_POOL_ID: userPoolId,
+			APP_CLIENT_ID: clientId,
+		},
+	  });
+	
 
+  
 
 
 	  
@@ -74,10 +112,14 @@ export class FloraVisionCloudStack extends cdk.Stack {
 	  // Grant Lambda permissions to interact with DynamoDB
 	wsConnectionTable.grantReadWriteData(disconnectWSLambda);
 
-
+	const authorizer = new agwa.WebSocketLambdaAuthorizer("Authorizer", authLambda, {
+		identitySource: [`route.request.querystring.idToken`],
+	  });
+  
 
 	const webSocketApi = new apigw2.WebSocketApi(this, 'websocket-api', {
 		connectRouteOptions: {
+		  authorizer,
 		  integration: new apigw2Integrations.WebSocketLambdaIntegration('ws-connect-integration', connectWSLambda),
 		},
 		disconnectRouteOptions: {
