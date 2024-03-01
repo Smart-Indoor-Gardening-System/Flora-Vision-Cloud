4e8819c3f70c4b68cb15a801974cb9344b6cc2da
erentrk01 <68688967+erentrk01@users.noreply.github.com>
2024-03-01 19:55:53 +0300

    Hashed Device password added

--- lambda/addDevice.ts
+++ lambda/addDevice.ts
@@ -1,20 +1,22 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
-import { QueryCommand, PutCommand,  GetCommand } from '@aws-sdk/lib-dynamodb';
+import { PutCommand } from '@aws-sdk/lib-dynamodb';
 const dynamodb = new DynamoDB
 
 export const handler = async (event: any, context: any): Promise<void> => {
 	try {
 	  console.log(event);
-	  const { DeviceID } = event;
+	  const { DeviceID, password } = event;
 
 	  console.log('Saving device:', DeviceID);
+	  
 
 	  await dynamodb.send(
 		new PutCommand({
 		  TableName: process.env.TABLE_NAME,
 		  Item: {
-			pk: `${DeviceID}`,
-			battery:'45'
+			pk: DeviceID,
+			password,
+			battery:'100'
 		  },
 		})
 	  );
